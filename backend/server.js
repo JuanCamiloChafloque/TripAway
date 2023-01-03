6f8f82a67a73d55e7394afaf139a7340960b5b12
Camilo <camilochafloque@gmail.com>
2023-01-03 15:03:50 -0600

    Backend express server and MongoDB setup

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const db = require("./config/database");
+
+//Dot ENV Init
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
+//Routes
+
+//Middlewares
+db();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//Other middlewares
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//Routes Initialization
+
+//Error middleware
+
+//Port listener
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running on " + process.env.NODE_ENV + " mode on port " + PORT
+  )
+);
+
+//Error handling for shutting down the server
+process.on("uncaughtException", (err) => {
+  console.log("Error: " + err.message);
+  console.log("Shutting down server due to uncaught rejection ");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("Error: " + err.message);
+  console.log("Shutting down server due to unhandled promise rejection ");
+  server.close(() => {
+    process.exit(1);
+  });
+});
