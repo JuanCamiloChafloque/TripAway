fcb26c943c3a77c18b65bb16a32279afa1c74268
Camilo <camilochafloque@gmail.com>
2023-01-04 14:56:57 -0600

    Initial Redux setup finished

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,8 @@
 import React from "react";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
@@ -8,9 +12,9 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
